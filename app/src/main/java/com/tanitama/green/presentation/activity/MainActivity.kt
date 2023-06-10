c2cb1b1b507389017befce3e0a46d496ecf92e24
rizkypsr <rizkypsrr@gmail.com>
2023-06-10 11:05:24 +0700

    Use Splash Screen New API 21

--- app/src/main/java/com/tanitama/green/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/tanitama/green/presentation/activity/MainActivity.kt
@@ -5,14 +5,21 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.tanitama.green.data.model.request.LoginUserRequestBody
 import com.tanitama.green.databinding.ActivityMainBinding
+import com.tanitama.green.presentation.viewmodel.AuthViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    private val authViewModel: AuthViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -26,6 +33,20 @@ class MainActivity : AppCompatActivity() {
 
         binding.toolbarIcon.setOnClickListener {
             clearSharedPreferences()
+        }
+
+        checkSession()
+    }
+
+    private fun checkSession() {
+        val sharedPref = getSharedPreferences("credentials", Context.MODE_PRIVATE)
+        val email = sharedPref.getString("email", null)
+        val password = sharedPref.getString("password", null)
+
+        if (!email.isNullOrEmpty() && !password.isNullOrEmpty()) {
+            authViewModel.loginUser(LoginUserRequestBody(email, password))
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
     }
 
